f34095ad3588e5736c59f0a968784a71da4849fd
Infinity9591 <son03.90658@gmail.com>
2023-01-31 20:52:54 +0700

    Fix Models

--- CocVl/Models/Students.cs
+++ CocVl/Models/Students.cs
@@ -2,7 +2,7 @@
 {
     public class Students
     {
-        public int StudentID { get; set; }
+        public string? StudentID { get; set; }
         public string? Name { get; set;}
         public string? Phone { get; set;}
         public string? Email { get; set;}
